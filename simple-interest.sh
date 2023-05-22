1e1164b54ed1c250c1ea2aa9ba15da06641fefc9
flotchet <flo.maisse@live.be>
2023-05-22 11:34:10 +0200

    modified User

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
        # Do not use this in production. Sample purpose only.
        # Author: Upkar Lidder (IBM)
        # Additional Authors:
-       # <Flotchet>
+       # Flotchet
        # Input:
        # p, principal amount
        # t, time period in years
